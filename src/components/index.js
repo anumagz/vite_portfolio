861b02c6585b576fdfb38c959e3c413b34372c1f
anumagz <anumagz1412@gmail.com>
2024-08-10 22:52:22 +0530

    added yml

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,7 @@ import Works from "./Works";
 import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
-import Illust from './illust';
+import Illust from './Illust';
 
 
 export {
