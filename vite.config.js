405677ba9523d18bd73d22192807ebecefe34eaf
anumagz <anumagz1412@gmail.com>
2024-08-10 23:36:57 +0530

    reduce into chunks

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+});
